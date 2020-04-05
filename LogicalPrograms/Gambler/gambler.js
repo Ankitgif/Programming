f5eca62001b5fa39946043e006f3484a87f85744
Ankitgif <ankitsinha2303@gmail.com>
2020-04-05 14:56:41 +0530

    [Ankit] Refactor, Added regex pattern and remove if-else with ternary operator

--- LogicalPrograms/Gambler/gambler.js
+++ LogicalPrograms/Gambler/gambler.js
@@ -15,15 +15,10 @@ let numberOftrial = input.questionInt("Enter number of trials : ");             
 let stake = input.questionInt("Enter stake in INR : ");                                  // stake
 let goal = input.questionInt("Enter goal in INR : ");                                    // goal
 let bet = 0, win = 0, loss = 0;
-const validate = () =>{                                                                 // function for validate
-    if(/[1-9]/.test(numberOftrial) && /[1-9]/.test(stake) && /[1-9]/.test(goal))        //Checking for integer value
-    {
-        gamblerMain(numberOftrial, stake, goal, bet, win, loss);                        //passing arguments to functions
-    }
-    else
-    {
-        console.log("Enter valid trials , stake and goal");
-    }
+const validate = () => {                                                                 // function for validate
+    let pattern = /[1-9]/
+    return pattern.test(numberOftrial) && pattern.test(stake) && pattern.test(goal) ? gamblerMain(numberOftrial, stake, goal, bet, win, loss)
+        : console.log("Enter valid trials , stake and goal");
 }
 validate();
 
