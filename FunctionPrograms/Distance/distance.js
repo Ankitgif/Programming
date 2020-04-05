e512bfd6c6591dcbf1e4f14d22f93d144f4f67bc
Ankitgif <ankitsinha2303@gmail.com>
2020-04-05 12:35:41 +0530

    [Ankit] Refactor, Removed if-else with ternary operator and unnecessary code

--- FunctionPrograms/Distance/distance.js
+++ FunctionPrograms/Distance/distance.js
@@ -11,16 +11,9 @@ const distanceMain = require('./distanceBl');                       //in which f
 const validate = () => {
     let point_x = input.questionInt("Enter value of point x : ");
     let point_y = input.questionInt("Enter value of point y : ");
-
-    if (/[1-9]/.test(point_x) || /[1-9]/.test(point_y)) {                              //checking two points are integer
-        Distance = distanceMain(point_x, point_y);                                      //passing two points as parameter
-        console.log(Distance.toFixed(2));
-
-    }
-    else {
-
-        console.log("Enter valid points");
-    }
+    let pattern = /^[1-9]$/
+    return pattern.test(point_x) || pattern.test(point_y) ? console.log(Distance = distanceMain(point_x, point_y)) : console.log("Enter valid points");
 
 }
+
 validate();
